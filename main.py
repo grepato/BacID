5ad0e39110a0387f014380bfd21667ba18061501
Grete <grete.paat@gmail.com>
2023-11-29 17:19:35 +0200

    liiginime järgi otsimine töötab

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ import bacdive
 
 client = bacdive.BacdiveClient('grete.paat@gmail.com', 'LBWLWeEjzD6y9!f')
 
+print("Tere tulemast!")
+print("Kasutad tarkvara, mis võimaldab bakterite identifitseerimist")
+print("Täida küsimuste väljad (kui tead) ja me anname sulle vasted")
+liiginimi = input("Sisesta liiginimi: ")
+märksõnad = input("Sisesta muid uuritava organismiga seotud märksõnu (eralda komaga): ").split(", ")    
+
+vastus = client.search(taxonomy = liiginimi)
+print("Leitud " + str(vastus) + " vastet.")
+
